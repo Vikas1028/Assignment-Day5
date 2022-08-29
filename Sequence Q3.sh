3819eaa523ee9e616ef2783d6977d81887c5e768
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-08-29 16:58:02 +0530

    Add Two Random Dice Number

---
+++ Sequence Q3.sh
@@ -0,0 +1,6 @@
+dice1=$((1+RANDOM%6))
+dice2=$((1+RANDOM%6))
+sum=$((dice1+dice2))
+echo "First random dice number  :" $dice1
+echo "Second random dice number  :" $dice2
+echo "Addition of two random dice numbers is :"$sum
