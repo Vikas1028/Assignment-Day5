4cf28884191bb712b3dc095be572227812e24e5e
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-02 13:30:33 +0530

    Unit Conversion 1)Feet in Inch 2) plot feet in meters 3) plot feet in acers

---
+++ sequence Q5..sh
@@ -0,0 +1,5 @@
+echo "Unit Conversion"
+echo "A) 1ft = 12in then 42in = " $((42/12))"ft approximately"
+echo "B) Rectangular Plot of 60 feet X 40 feet is equals to " $((2400*3048/1000>
+echo "C) Area of 25 such plots is equals to " $(((2400*25)/43560)) "acers appro>
+
